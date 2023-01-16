51b2c655b9661cac9020f09744e9058dde5e0fee
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-01-16 20:43:28 +0100

    #4 test projects init

---
+++ MultiPlanner.WebApp/MultiPlanner.LocalApi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MultiPlanner.LocalApi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
